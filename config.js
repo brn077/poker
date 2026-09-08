6621a9ba38e9a4104db79c30e5f2b2fd1a862477
brn077 <bbfigueiredo12@gmail.com>
2026-09-08 16:14:11 -0300

    Update Firebase configuration with new credentials

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 const firebaseConfig = {
-  apiKey:"COLE_AQUI",authDomain:"COLE_AQUI",databaseURL:"COLE_AQUI",
-  projectId:"COLE_AQUI",storageBucket:"COLE_AQUI",
-  messagingSenderId:"COLE_AQUI",appId:"COLE_AQUI"
+  apiKey:"AIzaSyBn3yOuOp8W6B2jFOaUNVvdwx8PCnW4SiA",
+  authDomain:"pokeer-4644a.firebaseapp.com",
+  databaseURL:"https://pokeer-4644a-default-rtdb.firebaseio.com",
+  projectId:"pokeer-4644a",
+  storageBucket:"pokeer-4644a.firebasestorage.app",
+  messagingSenderId:"500983172078",
+  appId:"1:500983172078:web:88bec8b4d5967d4ae65cff"
 };
